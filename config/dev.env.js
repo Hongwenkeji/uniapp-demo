1bd2b617e24121a5a5f0ec73eba8237b811542b0
刘跃文 <2382080246@qq.com>
2023-07-15 10:15:41 +0800

    模板

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,4 +1,4 @@
 module.exports = {
     NODE_ENV: 'development',
-    VUE_APP_BASE_URL: 'https://fc-mp-d03cc55d-46ff-4280-bcaa-1e62b07a0e4f.next.bspapp.com',
+    VUE_APP_BASE_URL: 'http://localhost:8080/api',
 }
